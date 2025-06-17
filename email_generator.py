763a913980876145d9004102dd526497e8e5f7ca
spacescribe <nandini2417@gmail.com>
2025-06-17 21:57:37 +0530

    Added basic web loader, llm_helper

--- email_generator.py
+++ email_generator.py
@@ -1,15 +1,4 @@
-from dotenv import load_dotenv
-import os
-from langchain_groq import ChatGroq
+from llm_helper import llm
 
-load_dotenv()
-
-API_KEY=os.getenv('GROQ_API_KEY')
-llm = ChatGroq(
-    model = "llama3-70b-8192",
-    api_key=API_KEY,
-    temperature=0.6,
-)
-
-response= llm.invoke("What is the most populated country in the world?")
+response=llm.invoke("What is the capital of India?")
 print(response.content)
---
+++ llm_helper.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+API_KEY=os.getenv('GROQ_API_KEY')
+llm = ChatGroq(
+    model = "llama3-70b-8192",
+    api_key=API_KEY,
+    temperature=0.6,
+)
