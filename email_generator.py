50c86ef3615f78a6ffaf6f3932059b9b8ce43d36
spacescribe <nandini2417@gmail.com>
2025-06-17 22:06:25 +0530

    Added prompt to extract job details into json

--- email_generator.py
+++ email_generator.py
@@ -1,4 +1,26 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
 from llm_helper import llm
+from web_loader import loader
 
-response=llm.invoke("What is the capital of India?")
-print(response.content)
+page_data = loader.load().pop().page_content
+parser = JsonOutputParser()
+
+prompt = PromptTemplate.from_template(
+    """
+        ### SCRAPED TEXT FROM WEBSITE:
+        {page_data}
+        ### INSTRUCTION:
+        The scraped text is from the career's page of a website.
+        Your job is to extract the job postings and return them in JSON format containing the 
+        following keys: `role`, `experience`, `skills` and `description`.
+        Only return the valid JSON.
+        ### VALID JSON (NO PREAMBLE):    
+    """
+)
+
+chain = prompt | llm
+res=chain.invoke(input={'page_data': page_data})
+
+parsed_data = parser.parse(res.content)
+print(parsed_data)
--- web_loader.py
+++ web_loader.py
@@ -1,7 +1,3 @@
 from langchain_community.document_loaders import WebBaseLoader
 
 loader = WebBaseLoader("https://careers.nike.com/lead-data-engineer-itc/job/R-61666?source=BY_Google_SEM&utm_source=BY_Google_SEM&utm_medium=employer_ad&utm_campaign=TACOE%20APLA_India%20&utm_content=NikeInc")
-
-page_data = loader.load().pop().page_content
-
-print(page_data)
