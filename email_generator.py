7d5f361d56ad3654ff0fe8acf0138561d29c8cf7
spacescribe <nandini2417@gmail.com>
2025-06-18 21:29:24 +0530

    Added chromadb persistent db

--- email_generator.py
+++ email_generator.py
@@ -2,6 +2,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from llm_helper import llm
 from web_loader import loader
+from db_helper import collection
 
 page_data = loader.load().pop().page_content
 parser = JsonOutputParser()
@@ -13,7 +14,7 @@ prompt = PromptTemplate.from_template(
         ### INSTRUCTION:
         The scraped text is from the career's page of a website.
         Your job is to extract the job postings and return them in JSON format containing the 
-        following keys: `role`, `experience`, `skills` and `description`.
+        following keys: `role`, `experience`, `skills` and `description`. Just give a single json object and not an unnecessary list of json.
         Only return the valid JSON.
         ### VALID JSON (NO PREAMBLE):    
     """
@@ -21,6 +22,7 @@ prompt = PromptTemplate.from_template(
 
 chain = prompt | llm
 res=chain.invoke(input={'page_data': page_data})
+job = parser.parse(res.content)
+print(job)
 
-parsed_data = parser.parse(res.content)
-print(parsed_data)
+
